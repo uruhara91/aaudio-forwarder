d4061a0d7d5fdb15dcf6a8a754fc6e3394b27f3f
uruhara91 <79046614+uruhara91@users.noreply.github.com>
2025-12-26 14:16:24 +0700

    Fix syntax

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,12 +64,12 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_1_8
-        targetCompatibility JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "1.8"
     }
 }
 
